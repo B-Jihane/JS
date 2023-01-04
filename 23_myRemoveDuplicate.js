9a5ba2c38f74f6f29c81323010c3832ecca79a6d
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-04 13:51:50 +0100

    suite exo tableaux

--- 23_myRemoveDuplicate.js
+++ 23_myRemoveDuplicate.js
@@ -1,15 +1,12 @@
-const myRemoveN = require ("./20_myRemoveN")
 function myRemoveDuplicate(myTable){
-    for(let i = 0; i < myTable.length; i++){
-        for(let j = i+1 ; j < myTable.length; j++){
-            if (myTable[i] === myTable[j]){
-                return myRemoveN(j)
-            }
+    let tableResults = [];
+    for (let i = 0; i < myTable.length; i++){
+        if (!tableResults.includes(myTable[i])){
+            tableResults.push(myTable[i]);
         }
-    }
+    } return tableResults;
 }
 
 module.exports = myRemoveDuplicate;
 
 
-console.log(myRemoveDuplicate([1, 2, 2, 3])); // [1, 2, 3]
---
+++ 24_myConcat.js
@@ -0,0 +1,6 @@
+function myConcat (table1, table2){
+    return table1.concat(table2);
+}
+
+module.exports = myConcat;
+
