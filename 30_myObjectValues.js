8ead983038c04f6caf2331944f2f0c88a82444f7
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-05 15:11:07 +0100

    modification exo 30

--- 30_myObjectValues.js
+++ 30_myObjectValues.js
@@ -11,6 +11,3 @@ function myObjectValues(myClass){
 
 
 module.exports = myObjectValues;
-
-console.log(myObjectValues({name: "Ganster", surname: "Guilian", age: 24, presentation: () => {}}));
-// "name: Ganster, surname: Guilian, age: 24"
